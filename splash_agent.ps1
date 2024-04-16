937c15cec862c0a47b83527ada430cc9bb94ead6
jamsub <85641501+jamsub@users.noreply.github.com>
2024-04-16 10:09:33 -0500

    Update splash_agent.ps1

--- splash_agent.ps1
+++ splash_agent.ps1
@@ -1,7 +1,7 @@
 # Define the directory where you want to install Splashtop Agent
 $installDirectory = "C:\users\public\splashtop"
 # Define the URL where you want to download Splashtop Agent
-$downloadUrl = "https://cloudbuild.splashtop.com/D5501271504299378C5F9062C69C2D78/2XKJSP4AHR4W/37a6259cc0c1dae299a7866489dff0bd/3.6.4.1/https://download.splashtop.com/csrs/Splashtop_Streamer_Win_DEPLOY_INSTALLER_v3.6.4.1.exe"
+$downloadUrl = https://download.splashtop.com/csrs/Splashtop_Streamer_Win_DEPLOY_INSTALLER_v3.6.4.1.exe"
 
 # Check if the installation directory already exists
 if (!(Test-Path -Path $installDirectory -PathType Container)) {
